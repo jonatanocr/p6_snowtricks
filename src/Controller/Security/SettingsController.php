eb1506c546a7422ee7cd90d8e9f621023884498e
jonatanocr <jmbzk@pm.me>
2021-11-05 14:00:41 +0100

    add change password in settings

--- src/Controller/Security/SettingsController.php
+++ src/Controller/Security/SettingsController.php
@@ -3,13 +3,16 @@
 namespace App\Controller\Security;
 
 
+use App\Entity\ChangePassword;
 use App\Entity\User;
 use App\Form\AccountInformationType;
+use App\Form\PasswordUpdateFormType;
 use App\Form\SettingsFormType;
 use App\Service\Notification\Notification;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 
@@ -21,24 +24,49 @@ class SettingsController extends AbstractController
         $this->security = $security;
     }
 
-    #[Route('/settings', name: 'settings')]
-    public function updateUser(Request $request): Response
+    /**
+     * @Route("/settings", name="settings")
+     */
+    public function updateUser(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
     {
         $user = new User();
         $entityManager = $this->getDoctrine()->getManager();
         $user = $entityManager->getRepository(User::class)->find($this->security->getUser()->getId());
-        $form = $this->createForm(SettingsFormType::class, $user);
+        $formSettings = $this->createForm(SettingsFormType::class, $user);
 
-        $form->handleRequest($request);
+        $changePassword = new ChangePassword();
+        $formPassword = $this->createForm(PasswordUpdateFormType::class, $changePassword);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        $formSettings->handleRequest($request);
+
+        if ($formSettings->isSubmitted() && $formSettings->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->redirectToRoute('settings');
+        }
+
+        $formPassword->handleRequest($request);
+
+        if ($formPassword->isSubmitted() && $formPassword->isValid()) {
+            $user->setPassword(
+                $userPasswordHasherInterface->hashPassword(
+                    $user,
+                    $formPassword->get('newPassword')->getData()
+                )
+            );
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+
             $entityManager->persist($user);
             $entityManager->flush();
             $this->redirectToRoute('settings');
         }
 
         return $this->render('settings/settings.html.twig', [
-            'settingsForm' => $form->createView(),
+            'settingsForm' => $formSettings->createView(),
+            'passwordForm' => $formPassword->createView(),
         ]);
 
     }
---
+++ src/Form/PasswordUpdateFormType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ChangePassword;
+use App\Entity\User;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PasswordUpdateFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('oldPassword', PasswordType::class, [
+                'attr' => ['placeholder' => 'Current password'],
+                'label' => false,
+            ])
+            ->add('newPassword', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => [
+                    'attr' =>['placeholder' => 'New Password'],
+                    'label' => false,
+                ],
+                'second_options' => [
+                    'attr' => ['placeholder' => 'Repeat Password'],
+                    'label' => false,
+                ],
+            ])
+            ->add('Save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ChangePassword::class,
+        ]);
+    }
+}
