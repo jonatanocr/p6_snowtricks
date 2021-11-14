d665596ee2b4f8ec17f27d3b2f5f140989f12688
jonatanocr <jmbzk@pm.me>
2021-11-14 17:30:13 +0100

    feat edit trick

---
+++ src/Controller/Tricks/UpdateTrickController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Controller\Tricks;
+
+
+use App\Entity\Trick;
+use App\Form\UpdateTrickFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UpdateTrickController extends AbstractController
+{
+    /**
+     * @Route("/trick_update/{id}", name="trick_update")
+     */
+    public function updateTrick(Request $request, int $id): Response
+    {
+        $trick = new Trick();
+        $entityManager = $this->getDoctrine()->getManager();
+        $trick = $entityManager->getRepository(Trick::class)->find($id);
+        $updateTrick = $this->createForm(UpdateTrickFormType::class, $trick);
+        $updateTrick->handleRequest($request);
+        if ($updateTrick->isSubmitted() && $updateTrick->isValid()) {
+            $entityManager->persist($trick);
+            $entityManager->flush();
+            $this->addFlash('success', 'Trick updated');
+            return $this->redirectToRoute('app_homepage');
+        }
+        return $this->render('tricks/update.html.twig', [
+            'updateTrickForm' => $updateTrick->createView(),
+        ]);
+    }
+}
